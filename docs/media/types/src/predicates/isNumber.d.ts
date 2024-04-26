283406faa00b5261f2322a527bb57b5a4905e5d5
MrAntix <aj@antix.co.uk>
2024-04-26 23:10:37 +0100

    docs: only on deploy

--- docs/media/types/src/predicates/isNumber.d.ts
+++
@@ -1,2 +0,0 @@
-/** checks values is a number or a string which can be parsed into a number */
-export declare function isNumber(a: any): boolean;
--- docs/media/types/src/validation/IErrors.d.ts
+++
@@ -1,4 +0,0 @@
-/** interface for validation results, i.e. errors */
-export interface IErrors {
-    [key: string]: IErrors | any;
-}
